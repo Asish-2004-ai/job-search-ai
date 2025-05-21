f1e9f489745b4ffd4075d95ba17548e47e7e5404
ASISH DALABEHERA <asishdalabehera375@gmail.com>
2025-05-21 17:23:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import Jobs from "./pages/Jobs";
 import Dashboard from "./pages/Dashboard";
